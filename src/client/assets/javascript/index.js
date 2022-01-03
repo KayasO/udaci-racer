11ae70fdabb09c8a876b35d6177b6f8908bedfa2
Okan Kayas <okan-kayas@web.de>
2022-01-03 18:14:17 +0100

    Added racer description

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -213,9 +213,9 @@ function renderRacerCard(racer) {
   return `
 		<li class="card podracer" id="${id}">
 			<h3>${driver_name}</h3>
-			<p>${top_speed}</p>
-			<p>${acceleration}</p>
-			<p>${handling}</p>
+			<p>Top Speed: ${top_speed}</p>
+			<p>Acceleration: ${acceleration}</p>
+			<p>Handling: ${handling}</p>
 		</li>
 	`
 }
